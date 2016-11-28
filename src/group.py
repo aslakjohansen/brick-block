108f4d42fbcd4a2043629707b6f2de49284b8372
Aslak Johansen <aslakj@gmail.com>
2016-11-28 09:02:28 +0100

    Cleanup

--- src/group.py
+++ src/group.py
@@ -19,24 +19,6 @@ class Group:
         print('~~~~~~~ types %s %s %s' % (str(type(entity)), str(type(namespace)), str(type(name))))
         return namespace, name, ns_prefix
     
-    # construct an entity in the target graph
-    def compose (self, g, target_namespace, taget_prefix, namespace, name, source_prefix):
-        ns = Namespace(target_namespace)
-        
-        # guard: make sure namepace is bound
-        if not namespace in map(lambda pair: pair[1], g):
-            g.bind(source_prefix, ns)
-        
-        # construct entity
-        return ns['' % (target_namespace)]
-    
-    # copy an entity from one graph to another
-    def copy_to_target (self, g, target_namespace, taget_prefix, entity):
-        namespace, name, source_prefix = self.decompose(entity)
-        print('%s %s %s' % (namespace, name, ns_prefix))
-        target_entity = self.compose(g, target_namespace, taget_prefix, namespace, name, source_prefix)
-        return target_entity
-    
     def instantiate (self, target_namespace, target_prefix):
         g = Graph()
         
@@ -94,91 +76,6 @@ class Group:
                 g.add( (mapping[sub], pred, mapping[obj]) )
         
         return g
-        
-        ########################################################################
-        
-        # find ports
-        q = 'SELECT ?port ?description WHERE { ?port rdf:type grp:Port . ?port grp:labeled ?description }'
-        ports = set(self.g.query(q))
-        print('ports:')
-        for (port, description) in ports:
-            print(' - %s "%s"' % (port, description))
-        print('')
-        
-        # find entities
-        q = 'SELECT ?entity ?group WHERE { ?group rdf:type grp:Group . ?entity grp:within ?group }'
-        entitygroups = set(self.g.query(q))
-        entities = set(map(lambda entry: entry[0], entitygroups))
-        print('entities:')
-        for entity, group in entitygroups:
-            print(' - %s of %s' % (entity, group))
-        print('')
-        
-        # find groups
-        groups = set(map(lambda entry: entry[1], entitygroups))
-        print('groups:')
-        for group in groups:
-            print(' - %s' % group)
-        print('')
-        
-        # find outer groups
-        outer_groups = list(filter(lambda group: group not in entities, groups))
-        print('outer groups:')
-        for group in outer_groups:
-            print(' - %s' % group)
-        print('')
-        
-        # find outer entities
-        outer_entities = list(filter(lambda entity: True in map(lambda group: (entity,group) in entitygroups,
-                                                                outer_groups),
-                                     entities))
-        print('outer entities:')
-        for entity in outer_entities:
-            print(' - %s' % entity)
-        print('')
-        
-        # find outer ports
-        outer_ports = list(filter(lambda entity: entity in map(lambda port: port[0], ports), outer_entities))
-        print('outer ports:')
-        for port in outer_ports:
-            print(' - %s' % port)
-        print('')
-        
-        # create translation table
-        outer_port = outer_ports[0]
-        print(outer_port)
-        print(type(outer_port))
-        print(dir(outer_port))
-#        print(help(outer_port.n3))
-        print(self.g.namespaces)
-        print(self.g.namespace_manager)
-        print('')
-        print('namespaces:')
-        for (ns_prefix, path) in self.g.namespace_manager.namespaces():
-            print(' - %9s %s' % (prefix, path))
-        print('')
-        print(dir(self.g.namespace_manager.store.prefix))
-#        print(self.g.namespace_manager.store.prefix.__class__)
-#        print(self.g.namespace_manager.store['http://www.w3.org/2001/XMLSchema#'])
-        print('')
-        
-        # copy entities and create translation table
-        print('copy and build:')
-        for entity in entities:
-            self.copy_to_target(g, target_namespace, taget_prefix, entity)
-#            match = self.g.namespace_manager.store.prefix(entity)
-#            print(match)
-#            if not match:
-#                sys.stderr.write('Error: Unable to lookup "%s"\n' % entity)
-#                continue
-#            
-#            pfx, uri = match
-#            print(' - %9s %s' % (pfx, uri))
-        print('')
-        
-        # copy relations
-        
-        # create and return portmap
     
 
 ################################################################################
